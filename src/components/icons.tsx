11ce501b72a8bb25e132c94d8d3f25c271af8554
yogyy <yogimr261@gmail.com>
2024-05-13 00:28:12 +0700

    feat: add icons

---
+++ src/components/icons.tsx
@@ -0,0 +1,47 @@
+export const MapPin = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+      <circle
+        cx="12"
+        cy="10"
+        r="3"
+      />
+    </svg>
+  );
+};
+
+export const Calendar = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path d="M8 2v4" />
+      <path d="M16 2v4" />
+      <rect
+        width="18"
+        height="18"
+        x="3"
+        y="4"
+        rx="2"
+      />
+      <path d="M3 10h18" />
+    </svg>
+  );
+};
